35001c0db974824b340cda37ce1a7438c099fa8b
Boden McHale <boden.mchale@gmail.com>
2023-05-25 15:45:01 -0400

    Updated site.

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,11 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Captain Awesome";
+export const SITE_TITLE = "Boden McHale";
 export const SITE_DESCRIPTION =
-  "Welcome to my blog! I write about modern web development.";
-export const TWITTER_HANDLE = "@yourtwitterhandle";
-export const MY_NAME = "Captain Awesome";
+  "Welcome to my portfolio! I write about various development and literary topics.";
+export const TWITTER_HANDLE = "@BodenMcHale";
+export const MY_NAME = "Boden McHale";
 
 // setup in astro.config.mjs
 const BASE_URL = new URL(import.meta.env.SITE);
